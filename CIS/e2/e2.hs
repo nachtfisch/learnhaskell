8656d1565aa26037dbd8877139ff6266c36107fa
Christian <ch.fish@gmail.com>
2015-04-25 22:17:13 +0200

    added second excercise

--- CIS/e2/e2.hs
+++ CIS/e2/e2.hs
@@ -15,3 +15,19 @@ parseWords _ = Unknown "unknown"
 
 parse :: String -> [LogMessage]
 parse x  = map (parseMessage) . lines $ x
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert a Leaf = Node Leaf a Leaf
+insert i@(LogMessage _ t1 _) (Node l cur@(LogMessage _ t2 _) r) 
+  | t1 <= t2 = Node (insert i l) (cur) r
+  | otherwise = Node l (cur) (insert i r)
+insert _ _ = error "insert into a tree containing unknown messages is not supported"
+
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build (h:r) = insert h (build r)
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = [] 
+inOrder (Node lt mt rt) = (inOrder lt) ++  [mt] ++ (inOrder rt) 
